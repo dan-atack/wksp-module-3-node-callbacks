67da2baee726378eff835768226dadaa15854e09
Nathalie Benarroch <nathaliebenarroch@Nathalies-MacBook-Pro.local>
2020-06-10 21:37:45 -0400

    finished workshop

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-'use strict';
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const { handleHomePage, handleFormData, handle404 } = require('./handlers');
+"use strict";
+//importng dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const { handleHomePage, handleFormData, handle404 } = require("./handlers");
 
 const PORT = process.env.PORT || 8000;
-
+// initializing express and below is middle
 express()
   .use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   })
-  .use(morgan('dev'))
-  .use(express.static('public'))
+  .use(morgan("dev"))
+  .use(express.static("public"))
   .use(bodyParser.json())
   .use(express.urlencoded({ extended: false }))
-  .set('view engine', 'ejs')
+  .set("view engine", "ejs")
 
   // endpoints
-  .get('/', handleHomePage)
-  .post('/form-data', handleFormData)
+  .get("/", handleHomePage)
+  .post("/form-data", handleFormData)
 
   // handle 404s
   .use(handle404)
